09204b8cc7bc4ac41df94e0a44efbcae566854eb
snippetWizard <paulsagar5433@gmail.com>
2023-12-11 23:26:04 +0530

    added NavBar component

--- src/data/Constants.jsx
+++ src/data/Constants.jsx
@@ -8,9 +8,8 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/snippetWizard,
-  resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  github: "https://github.com/snippetWizard",
+  resume:"https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/rishav_chanda/",
