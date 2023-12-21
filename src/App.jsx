fba6cdd1ef4287264d2588776bee078d643f122c
snippetWizard <paulsagar5433@gmail.com>
2023-12-21 23:16:48 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import { darkTheme, lightTheme } from './utils/Themes';
 import Navbar from './components/Navbar';
 import Hero from './components/HeroSection';
 import Skills from './components/Skills';
+import Experience from "./components/Experience";
 import Education from './components/Education';
 import Projects from './components/Projects';
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 const Body = styled.div`
@@ -37,13 +40,17 @@ console.log(openModal)
         <Hero />
         <Wrapper>
           <Skills />
-          <Education />
+          <Experience />
         </Wrapper>
         <Projects openModal={openModal} setOpenModal={setOpenModal} />
         <Wrapper>
-            {/* <Education />
-            <Contact /> */}
-          </Wrapper>
+            <Education />
+            <Contact />
+        </Wrapper>
+        <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
       </Body>
       </Router>
     </ThemeProvider>
